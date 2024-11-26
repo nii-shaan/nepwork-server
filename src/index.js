3210fb773495be58f5af4dae4785c3d6b7960908
Nishan Bista <nishanbista525@gmail.com>
2024-11-26 10:16:28 +0545

    db connection

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import dotenv from "dotenv/config";
 import { app } from "./app.js";
+import { connectDB } from "./db/connect.js";
 
-app.listen(8000, () => {
-  console.log("Server is running on port 8000");
-});
+const PORT = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.on("error", (error) => {
+      console.log(`Error on express >> \n${error}`);
+    });
+    app.listen(PORT, () => {
+      console.log(`server running on port : ${PORT} `);
+    });
+  })
+  .catch((error) => {
+    console.log(`failed to run server! ERROR> ${error}`);
+  });
 
 app.get("/", (req, res) => {
   res.json("This is nepwork server");
 });
+
+
+/**
+ * 
+ */
